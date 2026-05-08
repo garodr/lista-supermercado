d5c411852dfca272408cb7276a96f4710088b97e
Adrian Rodriguez <93204677+garodr@users.noreply.github.com>
2026-05-08 15:34:03 -0300

    modal moderno

--- vite.config.js
+++ vite.config.js
@@ -7,27 +7,33 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+
     VitePWA({
       registerType: 'autoUpdate',
+
       manifest: {
         name: 'Lista Supermercado',
         short_name: 'SuperLista',
+
         theme_color: '#ffffff',
         background_color: '#ffffff',
+
         display: 'standalone',
+
         icons: [
           {
             src: '/icon-192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
           },
+
           {
             src: '/icon-512.png',
             sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
 })
