4744bbbe8be564d578d034a90ed8647571c42d6a
Adrian Rodriguez <93204677+garodr@users.noreply.github.com>
2026-05-14 16:00:08 -0300

    editar producto 2

--- src/App.jsx
+++ src/App.jsx
@@ -375,7 +375,7 @@ export default function App() {
                           </strong>
                         </div>
                       )}
-                      <div className="flex items-center gap-42">
+                      <div className="flex items-center gap-3 ml-4">
                         <button
                           onClick={() => editarProducto(index)}
                           className="w-11 h-11 rounded-2xl bg-blue-500 text-white text-xl shadow-lg active:scale-95 transition"
