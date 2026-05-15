7ba51f6f2dc8e3613933b138efe8b92439824224
Adrian Rodriguez <93204677+garodr@users.noreply.github.com>
2026-05-15 10:41:03 -0300

    editar botones 2

--- src/App.jsx
+++ src/App.jsx
@@ -375,7 +375,7 @@ export default function App() {
                           </strong>
                         </div>
                       )}
-                      <div className="flex items-center justify-between w-44">
+                      <div className="flex items-center justify-between w-84">
                         <button
                           onClick={() => eliminarProducto(index)}
                           className="w-11 h-11 rounded-2xl bg-red-500 text-white text-xl shadow-lg active:scale-95 transition"
